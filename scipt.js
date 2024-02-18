76d0a5634639d343d80b2e6e15e7f5f165e84ad2
osmankiv <singsan324@gmail.com>
2024-02-18 14:11:41 +0200

    2/18/2024

--- scipt.js
+++ scipt.js
@@ -1,10 +1,27 @@
- let result = document.getElementById("GFG");
+let locationName = document.getElementById("locationName");
+//locationName.innerHTML = "location";
+let result = document.getElementById("GFG");
 
-    function changeColor(color) {
-      document.body.style.background = color;
-    }
+function changeColor(color) {
+  document.body.style.background = color;
+}
 
-    function myFunc() {
-      changeColor('yellow');
-      result.innerHTML = "Background Color changed";
-    }  
+function myFunc() {
+  changeColor('rgba(200, 200, 200, 2)');
+  // result.innerHTML = "Background Color changed";
+}
+let cantreName = `
+<div class="testOfThe">
+<h1>osman </h1>
+</div>
+`;
+
+console.log("\/osman\\\n\ / al\\\n\/smane\\");
+/////--- space of lerean ----/////
+
+let a = "-100";
+let b = "20";
+let c = 30;
+let d = true;
+console.log(); //2000
+console.log(b -c*a); //173
